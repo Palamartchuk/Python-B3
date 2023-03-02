596d3ada8300a06e4b7d1b602d085c05f49aee9a
matheus.lourenco <matheus.lourenco@2mi.com.br>
2023-03-02 19:55:35 -0300

    linux

--- b3async.py
+++ b3async.py
@@ -6,7 +6,7 @@ import requests
 
 async def make_request(row):
     try:
-        response = requests.post(
+        """ response = requests.post(
             url='http://127.0.0.1:8000/api/cadastraarquivo',
             data={
                 "email": row[14].lower().strip(),
@@ -16,16 +16,16 @@ async def make_request(row):
                 "arquivo": f'Comprovante IRRF 202201-202212 {row[1].lower().strip()}.pdf' if len(row[1].lower().strip()) > 11 else f'Comprovante IRRF 2022 {row[1].lower().strip()}.pdf'
             },
             verify=False
-        )
-        """ print(f'Nome :{row[2].lower().strip()} CPF : {row[1].lower().strip()}') """
-        print(response.status_code)
+        ) """
+        print(f'Nome :{row[2].lower().strip()} CPF : {row[1].lower().strip()}')
+        """ print(response.status_code) """
         """ print(f'Comprovante IRRF 202201-202212 {row[1].lower().strip()}.pdf') if len(row[1].lower().strip()) > 11 else print(f'Comprovante IRRF 2022 {row[1].lower().strip()}.pdf') """
     except:
         pass
 
 
 async def main():
-    with open('newb3.txt') as csv_file:
+    with open('newb3.txt', encoding='latin-1') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         next(csv_reader)
         line_count = 0
